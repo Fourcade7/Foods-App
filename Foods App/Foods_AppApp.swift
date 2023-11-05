05d370a0072b7d641bf9b982cebc5b093ff62143
fourcade7 <FourcadeAminov7@gmail.com>
2023-11-05 22:37:32 +0500

    Initial Commit

---
+++ Foods App/Foods_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foods_AppApp.swift
+//  Foods App
+//
+//  Created by Fourcade7 on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Foods_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
